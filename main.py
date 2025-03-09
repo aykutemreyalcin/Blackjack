2d32b74c796d8acff0be68a5c3439980bda4c57c
aykutemreyalcin <aykutemyeyalcinn@gmail.com>
2025-03-09 21:19:03 +0100

    adding some more lines

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import os
 import random
 #int(cards[0][:2])
 #int(cards[0][:1])
+os.system('cls||clear')
+time.sleep(0.5)
 print((10*'-')+"BlackJack"+(10*'-'))
-time.sleep(.5)
+time.sleep(1.5)
 
 balance = 100
 print(f"your balace is {balance}$")
-time.sleep(.5)
+time.sleep(1.5)
 os.system('cls||clear')
 
 deck_num = int(input("how many deck of cards do you want to play with?\n"))
@@ -41,13 +43,10 @@ for i in range(0, deck_num):
     for j in cards:
         cards_in_game.append(j)
 
-for i in table:
-    print(i)
-
 def print_table(balance, dealer_cards,dealer_cards_num, player_cards, player_cards_num, bet):
     print(f"""____________________   balance: {balance}$
 |                  |
-|   {dealer_cards}{dealer_cards_num}               
+|   {dealer_cards}  {dealer_cards_num}              
 |                  |
 |                  |
 |                  |
@@ -56,40 +55,94 @@ def print_table(balance, dealer_cards,dealer_cards_num, player_cards, player_car
 |                  |
 |                  |
 |                  |
-|   {player_cards}{player_cards_num}          {bet}$  
-____________________""")
-    
-def print_table_A(balance, dealer_cards,dealer_cards_num, player_cards, player_cards_num, bet):
-    print(f"""____________________   balance: {balance}$
-|                  |
-|   {dealer_cards}  {dealer_cards_num}               
-|                  |
-|                  |
-|                  |
-|                  |
-|                  |
-|                  |
-|                  |
-|                  |
-|   {player_cards}  {player_cards_num}/{player_cards_num_A}         {bet}$  
+|   {player_cards}  {player_cards_num}         {bet}$  
 ____________________""")
 
-print(cards_in_game)
-#2 11
-print_table(100,1,1,1,1,1)
-#while balance > 0:
-dealer_cards = []
-player_cards = []
-dealer_cards_num = 0
-player_cards_num = 0
-player_cards_num_A = 0
+os.system('cls||clear')
 
 while balance > 0:
+
+    dealer_cards = []
+    player_cards = []
+    dealer_cards_num = 0
+    player_cards_num = 0
+    player_cards_num_A = 0
+    dealer_cards_num_A = 0
+
+    print_table(balance,'','','','','')
+    bet = int(input("Please Put Your Bet!!!\n"))
+    balance -= bet
+    os.system('cls||clear')
+    print_table(balance,'','','','',bet)
+    time.sleep(1.5)
+    os.system('cls||clear')
+
     player_cards.append(cards_in_game.pop(random.randint(0, (len(cards_in_game)-1))))
     if player_cards[0][0] == 'A':
         player_cards_num += 11
         player_cards_num_A += 1
+    elif player_cards[0][0] == 'J' or player_cards[0][0] == 'Q' or player_cards[0][0] == 'K':
+        player_cards_num += 10
     elif len(player_cards[0]) == 3:
         player_cards_num += int(player_cards[0][:2])
     else:
-        player_cards_num += int(player_cards[0][:2])
+        player_cards_num += int(player_cards[0][:1])
+    
+    print_table(balance, dealer_cards, dealer_cards_num, player_cards, player_cards_num, bet)
+    time.sleep(1.5)
+    os.system('cls||clear')
+
+    dealer_cards.append(cards_in_game.pop(random.randint(0, (len(cards_in_game)-1))))
+    if dealer_cards[0][0] == 'A':
+        dealer_cards_num += 11
+        dealer_cards_num_A += 1
+    elif dealer_cards[0][0] == 'J' or dealer_cards[0][0] == 'Q' or dealer_cards[0][0] == 'K':
+        dealer_cards_num += 10
+    elif len(dealer_cards[0]) == 3:
+        dealer_cards_num += int(dealer_cards[0][:2])
+    else:
+        dealer_cards_num += int(dealer_cards[0][:1])
+    
+    print_table(balance, dealer_cards, dealer_cards_num, player_cards, player_cards_num, bet)
+
+    time.sleep(1.5)
+    os.system('cls||clear')
+
+    player_cards.append(cards_in_game.pop(random.randint(0, (len(cards_in_game)-1))))
+    if player_cards[1][0] == 'A':
+        player_cards_num += 11
+        player_cards_num_A += 1
+    elif player_cards [1][0] == 'J' or player_cards [1][0] == 'Q' or player_cards [1][0] == 'K':
+        player_cards_num += 10
+    elif len(player_cards[1]) == 3:
+        player_cards_num += int(player_cards[1][:2])
+    else:
+        player_cards_num += int(player_cards[1][:1])
+
+    print_table(balance, dealer_cards, dealer_cards_num, player_cards, player_cards_num, bet)
+    time.sleep(1.5)
+    os.system('cls||clear')
+    
+    dealer_cards.append(cards_in_game.pop(random.randint(0, (len(cards_in_game)-1))))
+    if dealer_cards[1][0] == 'A':
+        dealer_cards_num += 11
+        dealer_cards_num_A += 1
+    elif dealer_cards[1][0] == 'J' or dealer_cards[1][0] == 'Q' or dealer_cards[1][0] == 'K':
+        dealer_cards_num += 10
+    elif len(dealer_cards[1]) == 3:
+        dealer_cards_num += int(dealer_cards[1][:2])
+    else:
+        dealer_cards_num += int(dealer_cards[1][:1])
+    
+    print_table(balance, dealer_cards, dealer_cards_num, player_cards, player_cards_num, bet)
+
+    time.sleep(2)
+
+    if dealer_cards_num == 21 and player_cards_num == 21:
+        print("--DRAW--")
+        balance += bet
+    elif dealer_cards_num == 21:
+        print("--YOU LOST--")
+    continue
+
+
